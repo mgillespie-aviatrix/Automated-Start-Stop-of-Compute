d0e4c07a035e1e0d35faabfcf33b78522aef20b6
Matthew Gillespie <mgillespie+GIT@aviatrix.com>
2022-05-23 08:07:41 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Automated-Start-Stop-of-Compute
 This repository contains a simple python script leveraging boto3 which can be deployed as a Lamba in AWS to automate the startup/shutdown of select EC2 instances on a scheduled basis. 
+
+Included in this repository is a JSON IAM policy to apply to the lambda Execution Role (providing the requried access to describe regions and instances, as well as start/stop them)
---
+++ lambda_fnction.py
@@ -0,0 +1,63 @@
+#This lambda function utilizes the boto3 python module for AWS.
+# The function iterates over every available region in AWS searching for running EC2 instances that have been
+# tagged by the end user for regular daily shutdown. Once found, it will simply start or stop the EC2 instance depending upon the JSON payload sent to the function.
+#
+# @author: Matthew Gillespie
+# @date: 2021-05-10
+
+import json 
+import boto3
+import time
+
+def lambda_handler(event,context):
+    ec2 = boto3.client('ec2')
+    region_lookup = ec2.describe_regions()
+    response_dict = {}
+
+    #Check received JSON payload to determine requested action (start or stop ec2 instances)
+    action = event.get('operation')
+    print("IT IS x%sx " % action)
+    if action is None:
+        response_dict["status_code"] = 400
+        response_dict["message"] = "No operation received"
+        return response_dict
+    elif action not in ['start', 'stop']:
+        response_dict["status_code"] = 400
+        response_dict["message"] = "Operation is unknown (not start or stop)"
+        return response_dict
+
+    #Next, loop through all of the available regions to ensure we address all instances.
+    for r in region_lookup['Regions']:
+
+        #Search for ec2 instances in the region for our defined tag. 
+        filters=[{'Name' : 'tag:Automated-Shutdown-Enabled', 'Values': ['true']}]
+        ec2_lookup = boto3.resource('ec2', region_name=r['RegionName'])
+        instances = ec2_lookup.instances.filter(Filters=filters)
+        
+        #Loop through each instance matching the "Automated-Shutdown-Enabled = true" tag.
+        for i in instances:
+            #Determine name tag for this instance.
+            instance_name = "Unnamed Instance"
+            for tag in i.tags:
+                if 'Name' in tag['Key']:
+                    instance_name = tag['Value']
+                    break
+                
+            #We establish a dict to report back to the end-user the before/after state of each VM.
+            instance_dict = {}
+            instance_dict["id"] = i.id
+            instance_dict["name"] = instance_name
+            instance_dict["previous_state"] = i.state
+            
+            if action == "start":
+                i.start()
+            else:
+                i.stop()
+                
+            time.sleep(1)
+            instance_dict["new_state"] = i.state
+            response_dict[i.id] = instance_dict
+            
+    
+    response_dict["status_code"] = 200
+    return response_dict
